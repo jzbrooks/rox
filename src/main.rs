da67f9bdcb6a0583da8fe68305de1347af1fc2b1
Justin Brooks <justinbrooks@fastmail.com>
2021-06-25 20:55:12 -0500

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ mod vm;
 
 use bytecode::op_code;
 use bytecode::Chunk;
-use scanner::Scanner;
-use scanner::TokenKind;
 use std::env;
 use std::fs;
 use std::io;
@@ -15,42 +13,18 @@ use vm::InterpretResult;
 use vm::VM;
 
 fn main() {
-    // let mut scanner = Scanner::new("{}  ; .!=\n;{}\"test\"1000.03\ngorgonzola\nand\nbrie");
-    // let argv: Vec<String> = env::args().collect();
-    // let argc = argv.len();
+    let argv: Vec<String> = env::args().collect();
+    let argc = argv.len();
+    let vm = VM::new();
 
-    // let mut current = scanner.next();
-    // println!("{:?}", scanner.next());
-    // while current.kind != TokenKind::End {
-    //     current = scanner.next();
-    //     println!("{:?}", current);
-    // }
-
-    // let mut vm = VM::new();
-
-    // if argc == 1 {
-    //     repl(vm);
-    // } else if argc == 2 {
-    //     run_file(vm, &argv[1]);
-    // } else {
-    //     eprintln!("Usage: rox [path]");
-    //     process::exit(74);
-    // }
-    let chunk = Chunk::new(
-        vec![
-            op_code::CONSTANT,
-            1,
-            op_code::CONSTANT,
-            0,
-            op_code::DIVIDE,
-            op_code::NEGATE,
-            op_code::RETURN,
-        ],
-        vec![100.0, 5.0],
-        vec![123, 123, 123, 123, 123, 123, 123],
-    );
-    let mut vm = VM::new();
-    vm.interpret(chunk);
+    if argc == 1 {
+        repl(vm);
+    } else if argc == 2 {
+        run_file(vm, &argv[1]);
+    } else {
+        eprintln!("Usage: rox [path]");
+        process::exit(74);
+    }
 }
 
 fn repl(mut vm: VM) {
